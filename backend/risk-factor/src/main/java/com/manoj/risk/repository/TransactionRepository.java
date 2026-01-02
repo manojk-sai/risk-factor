627098c183b8c9d9475cdbb6f2cd739d81b89288
Manoj Sai Kothapati <manojsaikothapati@gmail.com>
2026-01-02 10:27:48 -0600

    Delete backend/risk-factor directory

--- backend/risk-factor/src/main/java/com/manoj/risk/repository/TransactionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.manoj.risk.repository;
-
-import com.manoj.risk.model.Transaction;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TransactionRepository extends MongoRepository<Transaction, String> {}

1bd4e6ed75152ecbe811f0c037df2edd96cd9ff8
manojk-sai <manojsaikothapati@gmail.com>
2026-01-03 10:34:53 -0600

    Test Cases Modification

---
+++ risk-factor/src/test/java/com/manoj/risk/RiskFactorApplicationTest.java
@@ -0,0 +1,11 @@
+package com.manoj.risk;
+
+import org.junit.jupiter.api.Test;
+
+class RiskFactorApplicationTest {
+  @Test
+  void applicationContextLoads() {
+    // Integration testing is covered by TransactionControllerTest
+    // This placeholder ensures the test suite is not empty
+  }
+}
--- risk-factor/src/test/java/com/manoj/risk/controller/TransactionControllerTest.java
+++ risk-factor/src/test/java/com/manoj/risk/controller/TransactionControllerTest.java
@@ -13,11 +13,12 @@ import com.manoj.risk.service.TransactionService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
 import java.time.Instant;
+import org.mockito.Mock;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(TransactionController.class)
@@ -27,7 +28,7 @@ class TransactionControllerTest {
 
   @Autowired private ObjectMapper objectMapper;
 
-  @MockBean private TransactionService transactionService;
+  @MockitoBean  private TransactionService transactionService;
 
   @Test
   void submitReturnsAssessment() throws Exception {
